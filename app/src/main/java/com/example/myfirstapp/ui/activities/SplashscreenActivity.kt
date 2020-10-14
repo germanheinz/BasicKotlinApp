517f5108e0689b5979cf3a4c69fcd44c69d33dcb
germanheinz <heinz.german@gmail.com>
2020-10-14 17:45:35 +0200

    Fragments and Activities created

---
+++ app/src/main/java/com/example/myfirstapp/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myfirstapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfirstapp.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
