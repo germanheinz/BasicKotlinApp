e2ff63a3e5228db083646fe65af636c7635cf9f5
germanheinz <heinz.german@gmail.com>
2020-10-15 18:01:56 +0200

    Creating Adapter from Scratch

---
+++ app/src/main/java/com/example/myfirstapp/view/adapter/ScheduleAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.myfirstapp.view.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ScheduleAdapter(): RecyclerView.Adapter<ScheduleAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduleAdapter.ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: ScheduleAdapter.ViewHolder, position: Int) {
+
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        
+    }
+}
