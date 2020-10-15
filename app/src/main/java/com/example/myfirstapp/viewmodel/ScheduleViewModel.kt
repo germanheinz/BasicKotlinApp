169116a9f1b55e8bb6d4a964275fe7ab7582edbc
germanheinz <heinz.german@gmail.com>
2020-10-15 17:49:12 +0200

    ViewModel - Schedule

---
+++ app/src/main/java/com/example/myfirstapp/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.myfirstapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.example.myfirstapp.model.Conference
+import com.example.myfirstapp.network.Callback
+import com.example.myfirstapp.network.FirestoreService
+
+class ScheduleViewModel {
+    val firestoreService = FirestoreService()
+    var listSchedule: MutableLiveData<List<Conference>> = MutableLiveData()
+    var isLoading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        getScheduleFromFirebase()
+    }
+
+    private fun getScheduleFromFirebase() {
+        firestoreService.getSchedule(object: Callback<List<Conference>>{
+            override fun onSuccess(result: List<Conference>?) {
+                listSchedule.postValue(result)
+                processFinished()
+            }
+            override fun onFailed(result: List<Conference>?) {
+                processFinished()
+            }
+        })
+    }
+
+    private fun processFinished() {
+        isLoading.value = true
+    }
+}
